d70186fdba6c13a94d521b21532671a8640b36b3
Thomas <broine.thomas@gmail.com>
2024-03-06 08:44:32 +0100

    Add : Channel component

--- src/components/Channel.tsx
+++ src/components/Channel.tsx
@@ -1,3 +1,38 @@
-import { Component } from "react";
+import React, { useState } from "react";
+import "./../style/index.css";
+import { Hash, Settings } from "lucide-react";
+import SettingsComponent from './SettingsChannel'; 
 
-Component
+function Channel(props: { id: string }) {
+  const [showSettings, setShowSettings] = useState(false);
+
+  const handleClose = () => {
+    setShowSettings(false);
+  };
+
+  return (
+    <li>
+      <div className="flex flex-row items-center h-11 focus:outline-none hover:bg-purple-100 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-black pr-6">
+        <a href="#channel1" className="flex items-center">
+          <span className="inline-flex justify-center items-center ml-4">
+            <Hash color="black" size={20} />
+          </span>
+          <span className="ml-2 text-sm tracking-wide truncate">
+            Channel {props.id}
+          </span>
+        </a>
+        <button
+          onClick={() => setShowSettings(true)}
+          className="ml-auto inline-flex justify-center items-center"
+        >
+          <span className="">
+            <Settings color="black" size={20} />
+          </span>
+        </button>
+      </div>
+      {showSettings && <SettingsComponent onClose={handleClose} />}
+    </li>
+  );
+}
+
+export default Channel;
