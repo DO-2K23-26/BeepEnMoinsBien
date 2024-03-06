b059676c1f821e70f79e53f0688a91724a0f5519
Thomas <broine.thomas@gmail.com>
2024-03-06 08:45:53 +0100

    Add : SettingsChannel component

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import '../style/App.css';
+import Sidebar from '../components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>App</h1>
-      </header>
+      <Sidebar />
     </div>
   );
 }
