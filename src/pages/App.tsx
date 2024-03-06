60b0e1c7aefd08bfb6c7c04edfcebc11ed6dd265
Thomas <broine.thomas@gmail.com>
2024-03-06 08:43:08 +0100

    Add : architecture

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
