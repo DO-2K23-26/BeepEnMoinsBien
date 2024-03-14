1d6c0dc11b654e269a1737e81da66b553a983ea0
Thomas <broine.thomas@gmail.com>
2024-03-14 15:00:50 +0100

    Update : Front Chatbox

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import { Socket, io } from 'socket.io-client';
 import env from "react-dotenv";
+import { SendHorizonal, Plus, User } from "lucide-react";
 
 function ChatBox() {
   const socket = useRef(null as unknown as Socket);
@@ -25,26 +26,53 @@ function ChatBox() {
   };
   
     return (
-      <div className="chat-container flex flex-col h-screen w-full border border-black">
+      <div className="chat-container flex flex-col h-screen  bg-violet-200 w-full">
         <div className="chat-header p-2 ">
-          <h2>Channel Name</h2>
+          <h2># Channel Name</h2>
         </div>
         <div className="chat-messages flex-grow overflow-y-auto p-2">
           <div className="message mb-2">
-            <strong>User1:</strong> Hello!
+            <div className="flex mb-2">
+              <div>
+                <span className="w-10 h-10 rounded bg-slate-200 ">
+                  <User color="black" size={30} />
+                </span>
+              </div>
+              <div>
+                <strong>User1</strong>
+              </div>
+            </div>
+
+            <div className="mr-7 bg-white rounded-lg p-4 text-black inline-block">
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+              sint temporibus odio, eligendi, ab delectus eos repellendus
+              tempore modi obcaecati adipisci magnam? Magnam laborum enim, eius
+              facere error, asperiores pariatur temporibus voluptatum nihil
+              minus sint. Natus a tempore voluptatem excepturi doloribus eum
+              inventore corrupti porro quisquam, error atque qui quam, esse
+              repudiandae architecto omnis reiciendis doloremque ipsa recusandae
+              molestiae exercitationem. Maxime culpa quasi, officia obcaecati
+              repellendus atque illo corrupti dolorum debitis totam amet in
+              possimus maiores voluptatum rerum qui beatae aspernatur quisquam
+              explicabo consequuntur enim nesciunt aliquid? Debitis placeat
+              eveniet officiis beatae, sed blanditiis ex accusamus saepe autem
+              distinctio ipsum.
+            </div>
           </div>
-          <div className="message mb-2">
-            <strong>User2:</strong> Hi, User1!
-          </div>
-          {/* Add more messages here */}
+
         </div>
-        <div className="chat-input flex items-center p-2 border-t border-black">
-          <input type="text" className="flex-grow box-border mr-2" placeholder="Type a message..." />
-          <button className="mr-2" onClick={handleClick}>
-            Send
+        <div className="chat-input flex items-center p-4 ">
+          <input
+            type="text"
+            className="flex-grow mr-7 bg-white rounded-lg p-4 text-black"
+            placeholder="Type a message..."
+          />
+          <button className="mr-7 bg-white rounded-lg p-4" onClick={handleClick}>
+            <SendHorizonal size={20} />
           </button>
-          <button>
-            Add
+          <button className="mr-2 bg-white rounded-lg p-4">
+            <Plus size={20} />
           </button>
         </div>
       </div>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,7 +6,7 @@ import ChannelList from "./ChannelList";
 function Sidebar() {
   return (
     <div className="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-50 text-gray-800">
-      <div className="h-screen flex flex-col top-0 left-0 w-80 bg-purple-200 h-full border-r justify-between">
+      <div className="h-screen flex flex-col top-0 left-0 w-80 bg-violet-300 h-full border-r justify-between">
         <div className="flex flex-col h-full">
           {/* Titre du serveur */}
           <div className="flex items-center justify-center h-14 border-b border-black">
