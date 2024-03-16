1018d408a4d5cb7975c5627768765f9b8442f1ae
Dorian TETU <dorian.tetu@etu.umontpellier.fr>
2024-03-16 21:13:47 +0100

    on peut join des channels les frerots

--- src/components/ChannelList.tsx
+++ src/components/ChannelList.tsx
@@ -4,19 +4,30 @@ import Channel from "./Channel";
 import { api_url } from "../context/envar";
 import { useContext, useEffect, useState } from "react";
 import { Plus } from "lucide-react";
+import JoinChannel from "./JoinChannel";
 
 function ChannelList() {
   const url = useContext(api_url);
   const [channels, setChannels] = useState([]);
+  const [showSettings, setShowSettings] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get(url + '/groupe');
-      setChannels(response.data);
+      try {
+        const response = await axios.get(url + '/groupe');
+        setChannels(response.data);
+      }
+      catch (error) {
+        console.error(error);
+      }
     };
 
     fetchData();
   }, [url]);
+
+  const handleClose = () => {
+    setShowSettings(false);
+  };
    
   return (
     <ul className="flex flex-col py-4 space-y-1">
@@ -25,7 +36,7 @@ function ChannelList() {
         <div className="text-sm font-light tracking-wide text-gray-500">
           Channels
         </div>
-        <button className="ml-auto inline-flex justify-center items-center">
+        <button className="ml-auto inline-flex justify-center items-center" onClick={() => setShowSettings(true)}>
           <span className=" ">
             <Plus color="black" size={20} />
           </span>
@@ -33,6 +44,7 @@ function ChannelList() {
       </div>
     </li>
     {channels.map((channel: {id: number, nom: string}) => <Channel key={channel.id} id={channel.nom}/>)}
+    {showSettings && <JoinChannel onClose={handleClose} />}
   </ul>
   );
 }
