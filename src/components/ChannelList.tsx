a6b29f43317545bec31ce8fbd2510277b618ef50
Dorian TETU <dorian.tetu@etu.umontpellier.fr>
2024-03-16 18:41:27 +0100

    commence rejoindre les channels

--- src/components/ChannelList.tsx
+++ src/components/ChannelList.tsx
@@ -3,19 +3,20 @@ import "./../style/index.css";
 import Channel from "./Channel";
 import { api_url } from "../context/envar";
 import { useContext, useEffect, useState } from "react";
+import { Plus } from "lucide-react";
 
 function ChannelList() {
   const url = useContext(api_url);
   const [channels, setChannels] = useState([]);
 
-   useEffect(() => {
-     const fetchData = async () => {
-       const response = await axios.get(url + '/groupe');
-       setChannels(response.data);
-     };
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(url + '/groupe');
+      setChannels(response.data);
+    };
 
-     fetchData();
-   }, [url]);
+    fetchData();
+  }, [url]);
    
   return (
     <ul className="flex flex-col py-4 space-y-1">
@@ -24,6 +25,11 @@ function ChannelList() {
         <div className="text-sm font-light tracking-wide text-gray-500">
           Channels
         </div>
+        <button className="ml-auto inline-flex justify-center items-center">
+          <span className=" ">
+            <Plus color="black" size={20} />
+          </span>
+        </button>
       </div>
     </li>
     {channels.map((channel: {id: number, nom: string}) => <Channel key={channel.id} id={channel.nom}/>)}
