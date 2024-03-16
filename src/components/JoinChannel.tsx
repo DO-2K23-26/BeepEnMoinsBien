eb2f094136638f3c88f111f06ecbf7bc31d120ca
Dorian TETU <dorian.tetu@etu.umontpellier.fr>
2024-03-16 18:41:27 +0100

    commence rejoindre les channels

---
+++ src/components/JoinChannel.tsx
@@ -0,0 +1,28 @@
+import { X } from "lucide-react";
+
+function joinChannel( { onClose }: { onClose: () => void } ) {
+    return (
+        <div className="bg-black bg-opacity-50 fixed inset-0 flex items-center justify-center z-10">
+            <div className="bg-white p-8 rounded-lg shadow-lg max-w-lg h-auto w-full relative">
+                <button onClick={onClose} className="absolute top-2 right-2">
+                    <X size={24} />
+                </button>
+                <h1 className="text-xl font-semibold mb-4 text-left">Paramètres du channel</h1>
+                <div className="flex items-center mb-4">
+                    <label className="block text-gray-700 text-lg mr-3">
+                        Nom du channel :
+                    </label>
+                <input
+                    type=""
+                    className="flex-grow  px-4 py-2 border rounded-lg text-gray-700 focus:border-blue-500"
+                />
+                </div>
+                <button className="w-full bg-purple-300 text-white px-4 py-2 rounded-lg hover:bg-purple-400 focus:outline-none">
+                Sauvegarder
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default joinChannel;
