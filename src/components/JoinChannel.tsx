1883c7f0624b33a47ff6443b439507842daa90ce
Dorian TETU <dorian.tetu@etu.umontpellier.fr>
2024-03-16 21:13:47 +0100

    on peut join des channels les frerots

--- src/components/JoinChannel.tsx
+++ src/components/JoinChannel.tsx
@@ -1,28 +1,46 @@
+import axios from "axios";
+import { useContext } from "react";
+import { api_url } from "../context/envar";
 import { X } from "lucide-react";
 
-function joinChannel( { onClose }: { onClose: () => void } ) {
+function JoinChannel( { onClose }: Readonly<{ onClose: () => void }> ) {
+    const url = useContext(api_url);
+
+    const handleValidate = async () => {
+        const channel = document.getElementById('channelName') as HTMLInputElement;
+        try {
+            await axios.post(url + '/groupe/createAndJoin/' + channel.value );
+            onClose();
+            window.location.reload();
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <div className="bg-black bg-opacity-50 fixed inset-0 flex items-center justify-center z-10">
             <div className="bg-white p-8 rounded-lg shadow-lg max-w-lg h-auto w-full relative">
                 <button onClick={onClose} className="absolute top-2 right-2">
                     <X size={24} />
                 </button>
-                <h1 className="text-xl font-semibold mb-4 text-left">Paramètres du channel</h1>
+                <h1 className="text-xl font-semibold mb-4 text-left">Rejoindre/Créer un channel :</h1>
                 <div className="flex items-center mb-4">
                     <label className="block text-gray-700 text-lg mr-3">
                         Nom du channel :
                     </label>
                 <input
                     type=""
-                    className="flex-grow  px-4 py-2 border rounded-lg text-gray-700 focus:border-blue-500"
+                    className="flex-grow px-4 py-2 border rounded-lg text-gray-700 focus:border-blue-500"
+                    id="channelName"
                 />
                 </div>
-                <button className="w-full bg-purple-300 text-white px-4 py-2 rounded-lg hover:bg-purple-400 focus:outline-none">
-                Sauvegarder
+                <button className="w-full bg-purple-300 text-white px-4 py-2 rounded-lg hover:bg-purple-400 focus:outline-none" onClick={handleValidate}>
+                Valider
                 </button>
             </div>
         </div>
     )
 }
 
-export default joinChannel;
+export default JoinChannel;
--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,13 @@ import './style/index.css';
 import { App } from './pages/App';
 import reportWebVitals from './reportWebVitals';
 import UserContextProvider from './context/authcontext';
+import axios from 'axios';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('accessToken')}`;
 
 root.render(
   <React.StrictMode>
