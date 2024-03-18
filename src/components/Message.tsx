9a26cc5c6c73181019c0bf16b1d3edd4bd1505ff
Dorian TETU <dorian.tetu@etu.umontpellier.fr>
2024-03-18 21:42:01 +0100

    rebasage

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,7 +1,9 @@
-import { PencilLine, User } from "lucide-react";
-import { useContext, useEffect, useRef, useState } from "react";
-import { SocketContext } from "../context/socketcontext";
+import { PencilLine } from "lucide-react";
+import { useContext, useEffect, useRef, useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import TextareaAutosize from 'react-textarea-autosize';
 import { UserContext } from "../context/authcontext";
+import { SocketContext } from "../context/socketcontext";
 
 interface MessageProps {
     message: string;
@@ -10,37 +12,50 @@ interface MessageProps {
 }
 
 function Message({ message, author, id }: Readonly<MessageProps>) {
-    const [editMode, setEditMode] = useState(false);
-    const [visible, setVisible] = useState(false);
+
     const socketContext = useContext(SocketContext);
     const socket = useRef(socketContext?.socketValue);
-    const user = useContext(UserContext)?.user;
+    const user = useContext(UserContext).user;
+
+    const [editMode, setEditMode] = useState(false);
+    const [visible, setVisible] = useState(false);
+    const [messageState, setMessageState] = useState(message);
 
     const handleClick = () => {
         setEditMode(!editMode);
     }
+
+    const messageRef = useRef<HTMLDivElement>(null);
+
+    const handleMouseEnter = () => setVisible(true);
+    const handleMouseLeave = () => {
+        // setTimeout pour donner une petite latence avant de vérifier et cacher le bouton
+        // Cela permet de gérer les mouvements rapides de la souris entre le message et le bouton
+        setTimeout(() => {
+            // Si le curseur n'est pas sur le message ou le bouton, on cache le bouton
+            if (messageRef.current && !messageRef.current.contains(document.activeElement)) {
+                setVisible(false);
+            }
+        }, 200); // la latence peut être ajustée selon les besoins
+    };
 
     useEffect(() => {
         socket.current = socketContext?.socketValue;
 
         if (socket.current) {
             socket.current.on("edit", (msg: any) => {
-                if (msg.id === id) {
-                    const message = document.getElementById(id.toString())
-                    if (message) {
-                        message.innerHTML = msg.contenu;
-                    }
-                }
+                if (msg.id !== id) return;
+                setMessageState(msg.contenu);
             });
-          }
-      
-          return () => {
+        }
+
+        return () => {
             socket.current?.off("edit");
-          };
+        };
 
     }, [socketContext?.socketValue, id]);
 
-    const handleEdit = () => { 
+    const handleEdit = () => {
         const message = (document.getElementById('editZone') as HTMLInputElement)?.value;
         const payload = {
             contenu: message,
@@ -51,30 +66,54 @@ function Message({ message, author, id }: Readonly<MessageProps>) {
         }
         setEditMode(false);
     }
-    
-    return (
-        <div className="message mb-2 hover:bg-violet-100 relative" onMouseEnter={() => setVisible(true)} onMouseLeave={() => setVisible(false)}>
-            <div className="flex mb-2">
-                <div>
-                <span className="w-10 h-10 rounded bg-slate-200 ">
-                    <User color="black" size={30} />
-                </span>
-                </div>
-                <div>
-                <strong>{author}</strong>
-                </div>
-            </div>
 
-            <div className="bg-white rounded-lg p-4 text-black inline-block">
-                {!editMode && <p id={id.toString()}>{message}</p>}
-                {editMode && <input id='editZone' type="text" defaultValue={message} onKeyDown={(e) => e.key === 'Enter' && handleEdit()}/>}
-            </div>
-            {visible && user?.nickname === author && (
-                <button type="submit" className="mr-5 bg-white rounded-lg p-2 absolute right-0 -top-5" style={{ float: "right" }} onClick={handleClick}>
-                    <PencilLine size={20} />
-                </button>
-            )}
-        </div>
+    return (
+        <>
+            {author === user?.nickname ?
+                <div ref={messageRef} onMouseOver={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                    <div className="mb-2 flex flex-col items-end">
+                        <div className="text-xs text-gray-500 mb-1">{author}</div>
+                        <div className="flex items-center gap-2">
+                            {visible && (
+                                <button
+                                    className="bg-blue-300 text-white rounded-lg p-2 hover:bg-blue-400 transition-colors duration-300"
+                                    onClick={handleClick}
+                                    onMouseEnter={handleMouseEnter}
+                                    onMouseLeave={handleMouseLeave}
+                                >
+                                    <PencilLine size={20} />
+                                </button>
+                            )}
+                            {!editMode ?
+                                <div className="bg-blue-500 text-white rounded-lg py-2 px-4 break-words">
+                                    <ReactMarkdown>{messageState.replace(/\n/g, "  \n")}</ReactMarkdown>
+                                </div>
+                                :
+                                <p className="bg-blue-500 text-black rounded-lg py-2 px-4">
+                                    <TextareaAutosize
+                                        id='editZone'
+                                        className="w-full h-full text-black bg-blue-200 p-2 rounded border-2 border-blue-300 focus:outline-none focus:border-blue-500  resize-none"
+                                        defaultValue={messageState}
+                                        onKeyDown={(e) => e.key === 'Enter' && e.shiftKey === false && handleEdit()}
+                                        maxRows={4}
+                                    ></TextareaAutosize>
+                                </p>
+                                }
+
+                        </div>
+                    </div>
+                </div>
+                :
+                <div className="mb-2 flex justify-start items-start">
+                    <div className="text-left">
+                        <div className="text-xs text-gray-500 mb-1">{author}</div>
+                        <div className="bg-gray-200 text-gray-700 rounded-lg py-2 px-4 break-words">
+                            <ReactMarkdown>{messageState.replace(/\n/g, "  \n")}</ReactMarkdown>
+                        </div>
+                    </div>
+                </div>
+            }
+        </>
     );
 }
 
