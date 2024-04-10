a6e4030b322aa63ef8c8c60f5f71e94f7994ea7f
benoit-planche <benoit.planche@etu.umontpellier.fr>
2024-04-10 18:27:38 +0200

    Update axios configuration for token authentication

--- src/axios.tsx
+++ src/axios.tsx
@@ -1,14 +1,23 @@
 import axios from 'axios';
+import { useContext } from 'react';
+import { useUserContext } from './context/authcontext'; 
+import { api_url } from './context/envar';
+
+
+const url = useContext(api_url);
 
 export const customAxios = axios.create({
-  baseURL: "",
+  baseURL: url,
 });
+
+const { accessToken } = useUserContext();
+const { setToken } = useUserContext();
 
 
 customAxios.interceptors.request.use(
     
         async (config) => {
-            const token = localStorage.getItem('accessToken');
+            const token = accessToken;
             if (token) {
                 config.headers["Authorization"] = `Bearer ${token}`;
             }
@@ -22,18 +31,9 @@ customAxios.interceptors.request.use(
 // Function to refresh the token
 const refreshToken = async () => {
     try {
-      let url = '';
-        if (process.env.NODE_ENV === 'development') {
-            url = 'http://localhost:9644';
-        } else {
-            url = 'https://api.beep.gay';
-        }
-      const resp = await axios.post(url + "/auth/refresh", { refreshToken: localStorage.getItem('refreshToken') });
-      console.log("rep", resp.data.accessToken);
-      const token = resp.data;
-      console.log("Token refreshed");
-      localStorage.setItem('accessToken', token.accessToken);
-      localStorage.setItem('refreshToken', token.refreshToken);
+      const resp = await customAxios.get("auth/refresh");
+      const { token } = resp.data;
+      setToken(token.accessToken, token.refreshToken);
       return token;
     } catch (error) {
       console.error("Error refreshing token:", error);
